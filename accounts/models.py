f00847c8a8fd2f44872697303d59a420829c3d2d
cinnamontea <sofia.castro@ing.uchile.cl>
2023-06-29 16:52:17 -0400

    figured out how to call a function to generate default keys in user creation

--- accounts/models.py
+++ accounts/models.py
@@ -12,16 +12,20 @@ class CryptoKey(models.Model):
     key_status = models.CharField(choices=StatusChoices.choices, max_length=1, null=True)
     value = models.UUIDField(default=uuid.uuid4, editable=False)
 
-    def generate_key(self, ktype, kstatus):
-        self.key_type = ktype
-        self.key_status = kstatus
-        self.save()
+    def generate_anon_key(ktype):
+        return CryptoKey.objects.create(key_type=ktype, key_status=1) #PUBLIC=1, PRIVATE=2, ANON=1
+    
+    def generate_pka():
+        return CryptoKey.generate_anon_key(1)
+    
+    def generate_ska():
+        return CryptoKey.generate_anon_key(2)
 
 
 class CustomUser(AbstractUser):
     likes = models.TextField(blank=True, null=True) #(for now, to make it simple)
-    pka = models.OneToOneField(CryptoKey, related_name="pka", on_delete=models.CASCADE, editable=False)
-    ska = models.OneToOneField(CryptoKey, related_name="ska", on_delete=models.CASCADE, editable=False)
+    pka = models.OneToOneField(CryptoKey, related_name="pka", on_delete=models.CASCADE, editable=False, default=CryptoKey.generate_pka)
+    ska = models.OneToOneField(CryptoKey, related_name="ska", on_delete=models.CASCADE, editable=False, default=CryptoKey.generate_ska)
 
     def __str__(self):
         return self.username
