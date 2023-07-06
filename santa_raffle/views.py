b0bc109a3ed2ad02348c273818320d15bb76cd70
cinnamontea <sofia.castro@ing.uchile.cl>
2023-07-05 20:10:28 -0400

    published events can be seen on home_feed

--- santa_raffle/views.py
+++ santa_raffle/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Event
 
 # Create your views here.
 
 def home_feed(request):
-    return render(request, 'santa_raffle/home_feed.html', {})
+    #my_events = Event.objects.filter(organizer=)
+    events = Event.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'santa_raffle/home_feed.html', {'events': events} )
