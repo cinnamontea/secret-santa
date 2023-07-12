6de95f557389399e968b6f4cd9ce659eb0be8b28
cinnamontea <sofia.castro@ing.uchile.cl>
2023-07-12 18:50:55 -0400

    fixing error in previous commit (filtered by organizer)

--- santa_raffle/views.py
+++ santa_raffle/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
-from .models import Event
+from .models import Event, Participant, CustomUser
 
 # Create your views here.
 
@@ -8,13 +8,16 @@ def home_feed(request):
     user = None
     if request.user.is_authenticated:
         user = request.user
-    events = Event.objects.filter(organizer=user).order_by('created_date')
+    participant = user.participant_set.all().order_by('event__created_date')
+    events = participant.values('event__id', 'event__organizer', 'event__title')
+    for e in events:
+        e['event__organizer'] = CustomUser.objects.get(id=e['event__organizer'])
     return render(request, 'santa_raffle/home_feed.html', {'events': events} )
+
 
 def event_detail(request, pk):
     event = get_object_or_404(Event, pk=pk)
     members = event.get_members_id()
-
     return render(request, 'santa_raffle/event_detail.html', 
                   {'event': event,
                    'members': members,
