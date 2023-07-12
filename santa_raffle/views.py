f55938e295930b78ef7ce9355dd5e5908bacedd9
cinnamontea <sofia.castro@ing.uchile.cl>
2023-07-12 16:53:05 -0400

    created event_details view

--- santa_raffle/views.py
+++ santa_raffle/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Event
 
 # Create your views here.
 
 def home_feed(request):
-    #my_events = Event.objects.filter(organizer=)
     events = Event.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
     return render(request, 'santa_raffle/home_feed.html', {'events': events} )
+
+def event_detail(request, pk):
+    event = get_object_or_404(Event, pk=pk)
+    members = event.get_members_id()
+
+    return render(request, 'santa_raffle/event_detail.html', 
+                  {'event': event,
+                   'members': members,
+                   })
